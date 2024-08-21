79a42fbda8f79db2656d665ec553439dd572c0e2
ellis-coldren <catherine.e.coldren@gmail.com>
2024-08-21 13:44:46 -0400

    commenting

--- trytri.m
+++ trytri.m
@@ -1,29 +1,32 @@
-n = 15;r = 0.01;
 
-t = linspace(0, 2*pi, 1000);
+n = 15;r = 0.01; % frequency and radius of cage vertices
+t = linspace(0, 2*pi, 1000); % parameter value
 
-%Circle
+
+% -------------------- Circle ----------------------- %
 % radius = 0.75;
 % x_shape = radius*cos(t);
 % y_shape = radius*sin(t);
 
 
-%Heart
+% -------------------- Heart ----------------------- %
 x_shape = 0.08*(16*sin(t).^3);
 y_shape = 0.08*(13*cos(t) - 5*cos(2*t) - 2*cos(3*t) - cos(4*t));
 
-%Diamond
+% -------------------- Diamond ----------------------- %
 % a = 0.7;
 % x_shape = a*cos(t).^3;
 % y_shape = a*sin(t).^3;
 
-%Rose Curve
+% -------------------- Rose curve ----------------------- %
 % a = 0.6;
 % petals = 4;
 % x_shape = a*cos(petals.*t).*cos(t);
 % y_shape = a*cos(petals.*t).*sin(t);
 
-%Hypotrochoid(sometimes playing around messes up, see https://www.desmos.com/calculator/r8bsgvsszo)
+% -------------------- Hypotrochoid ----------------------- %
+%(sometimes playing around messes up, see https://www.desmos.com/calculator/r8bsgvsszo)
+
 % fr = 1.6;
 % rr = 0.2;
 % d = 0.5;
@@ -32,128 +35,129 @@ y_shape = 0.08*(13*cos(t) - 5*cos(2*t) - 2*cos(3*t) - cos(4*t));
 
 
 p = [x_shape', y_shape'];
+q = curvspace(p,n+1); % generates points that interpolate curve
+xx = q(:, 1); % x values of curve
+yy = q(:, 2); % y values of curve
+c = xx + 1i*yy; % curve as vector of complex coordinates
 
-q = curvspace(p,n+1);
-%disp(q);
-xx = q(:, 1);
-yy = q(:, 2);
-c = xx + 1i*yy;
-disp(c);
-disp(size(c));
-
-% c = exp(2i*pi*(1:n)/n);
-rr = r*ones(size(c));
+rr = r*ones(size(c)); % radii of cage vertices as vector
 N = max(0, round(4+.5*log10(r)));
 npts=3*N+2;
-cagePts_num = n*npts;
-xres = 5;
-yres = 5;
-
-% x = linspace(-3, 3, 10); y = linspace(-3, 3, 10);
+% cagePts_num = n*npts;
+% xres = 5;
+% yres = 5;
 
 
-z = exp(pi*1i*(-50:50)'/50);
-% x = [0.25, 0.75, 0.75, 0.25];
-% y = [0.6, 0.6, 1.2, 1.2];
-x = [-3, 3, 3, -3];
-y = [-3, -3, 3, 3];
-disp(c);
-pgon = polyshape({x, real(c(1)+rr(1)*z)}, {y, imag(c(1)+rr(1)*z)});
+z = exp(pi*1i*(-50:50)'/50); % vector of circle coordinates for cage vertices
+x = [-3, 3, 3, -3]; % boundary of region
+y = [-3, -3, 3, 3]; % boundary of region
+
+% https://www.mathworks.com/help/matlab/ref/polyshape.html %
+% ---------------- setting up cage vertices --------------------- %
+
+% connects vertices of x and y, adds hole where first cage vertex is (c(1) is the center of the cage point, + rr(1)*z draws a circle with radius rr(1) around it) %
+pgon = polyshape({x, real(c(1)+rr(1)*z)}, {y, imag(c(1)+rr(1)*z)}); 
 for j = 2:n
+    % adds another hole for each cage vertex %
     pgon = addboundary(pgon,real(c(j)+rr(j)*z),imag(c(j)+rr(j)*z));
 end
+
+% triangulates the region %
 tr = triangulation(pgon);
 points = tr.Points; %row number is vertex id
 connectivity = tr.ConnectivityList; %row number is triangle id, rows contain vertex id
-% neighbors = neighbors(tr, id); %row is triangle id, tows contain triangle ids
+
+% creates finite element model so that we can create a mesh %
+% https://www.mathworks.com/help/pde/ug/fegeometry.html
+
 gm = fegeometry(tr);
 gm = generateMesh(gm, Hmax = 0.2, Hmin= 0.001);
 numelements = size(gm.Mesh.Elements, 2);
 numnodes = size(gm.Mesh.Nodes, 2);
-% midpoints of the edge are the last 3 rows of the gm.Mesh.Elements
-% vertices of the element(triangle) are the first 3 rows of the
-% gm.Mesh.Elements
+        % elements is 6 x (number of triangles)
+        % the vertex ids of the triangles are in the first three rows of gm.Mesh.Elements
+        % midpoints of the edges of the triangle are the last 3 rows of the gm.Mesh.Elements
+        % vertices of the element(triangle) are the first 3 rows of gm.Mesh.Elements
 
-% finding cage nodes:
+% this finds which vertices of our mesh lie on a cage vertex:
 cagePts = findNodes(gm.Mesh,"radius",[real(c(1)), imag(c(1))], rr(1));
 for j = 1:n
     cagePts = [cagePts findNodes(gm.Mesh,"radius",[real(c(j)), imag(c(j))], rr(j))];
 end
 
+% this finds which vertices of our mesh lie on the boundary:
 boundaryPts = findNodes(gm.Mesh, "region", "Edge",[1 2 3 4]);
 
+
 neighborsMatrix = zeros(numnodes);
+%neighborsMatrix(i, j) = neighborsMatrix(j, i) == 1 if vertex i and j share an edge, and 0 otherwise
 cotangents = zeros(numnodes);
+% cotangents(i, j) = 1/2*cot(alpha(i, j)) + cot(beta(i, j))
+for i = 1:numelements %iterate through the triangles
+  v1_id = gm.Mesh.Elements(1, i); v2_id = gm.Mesh.Elements(2, i); v3_id = gm.Mesh.Elements(3, i); %get vertex id's of the triangle
 
-for i = 1:numelements
-  v1_id = gm.Mesh.Elements(1, i);
-  v2_id = gm.Mesh.Elements(2, i);
-  v3_id = gm.Mesh.Elements(3, i);
-
-  v1 = gm.Mesh.Nodes(:, v1_id);
-  v2 = gm.Mesh.Nodes(:, v2_id);
-  v3 = gm.Mesh.Nodes(:, v3_id);
+  v1 = gm.Mesh.Nodes(:, v1_id);v2 = gm.Mesh.Nodes(:, v2_id);v3 = gm.Mesh.Nodes(:, v3_id); % get vertex locations
   
-  neighborsMatrix(v1_id, v2_id) = 1;
+  neighborsMatrix(v1_id, v2_id) = 1;neighborsMatrix(v2_id, v3_id) = 1;neighborsMatrix(v3_id, v1_id) = 1;
+  neighborsMatrix(v2_id, v1_id) = 1;neighborsMatrix(v3_id, v2_id) = 1;neighborsMatrix(v1_id, v3_id) = 1;
+
+
   cotangents(v1_id, v2_id) = cotangents(v1_id, v2_id) + 0.5*cot(acos(dot(v1-v3, v2-v3)/(norm(v1-v3)*norm(v2-v3))));
   cotangents(v2_id, v1_id) = cotangents(v1_id, v2_id);
-  neighborsMatrix(v2_id, v3_id) = 1;
+  
   cotangents(v2_id, v3_id) = cotangents(v2_id, v3_id) + 0.5*cot(acos(dot(v2-v1, v3-v1)/(norm(v2-v1)*norm(v3-v1))));
   cotangents(v3_id, v2_id) = cotangents(v2_id, v3_id);
-  neighborsMatrix(v3_id, v1_id) = 1;
+  
   cotangents(v3_id, v1_id) = cotangents(v3_id, v1_id) + 0.5*cot(acos(dot(v3-v2, v1-v2)/(norm(v3-v2)*norm(v1-v2))));
   cotangents(v1_id, v3_id) = cotangents(v3_id, v1_id);
-  neighborsMatrix(v2_id, v1_id) = 1;
-  neighborsMatrix(v3_id, v2_id) = 1;
-  neighborsMatrix(v1_id, v3_id) = 1;
+  
 end
 
+% if row or column i is entirely 0, then vertex i is a midpoint
+%%%% NOTE: if vertex i is a midpoint, than any vertex with an id greater
+%%%% than i is also a midpoint %%%%
+% the following removes those midpoints for efficiency
 neighborsMatrix = neighborsMatrix(any(neighborsMatrix, 2), :); %removing rows of the middle point nodes
 neighborsMatrix = neighborsMatrix(:, any(neighborsMatrix, 1)); %removing columns of the middle point nodes
-
 cotangents = cotangents(any(cotangents, 2), :); %removing rows of the middle point nodes
 cotangents = cotangents(:, any(cotangents, 1)); %removing columns of the middle point nodes
-size(neighborsMatrix)
+
+%updates numnodes to exclude midpoints
 numnodes = size(neighborsMatrix, 1);
 
+% updates cagePts and boundaryPts to remove any midpoint vertices
 colstodelete = cagePts > numnodes;
 cagePts(colstodelete) = [];
 colstodelete = boundaryPts > numnodes;
 boundaryPts(colstodelete) = [];
 
-% Laplacian_umbrella = zeros(numnodes);
+% creating cotangent Laplacian
 Laplacian_cotangents = zeros(numnodes);
 for i = 1:numnodes
-    % normalize_value1 = sum(neighborsMatrix(i, :));
     normalize_value2 = sum(cotangents(i, :));
-    % Laplacian_umbrella(i, :) = -neighborsMatrix(i, :)/normalize_value1;
-    % Laplacian_umbrella(i:i) = 1;
     Laplacian_cotangents(i, :) = -cotangents(i, :);
     Laplacian_cotangents(i,i) = normalize_value2;
 end
 
-
-
-% solve x'Lx
-
-% figure
-% pdemesh(gm)
-% hold on
-% plot(gm.Mesh.Nodes(1,boundaryPts),gm.Mesh.Nodes(2,boundaryPts),"or","MarkerFaceColor","g")
-
 a = zeros(numnodes, 1);
 C = zeros(numnodes, numnodes);
+% C(i, i) = 1 if vertex i is on the boundary
 for i =1:size(boundaryPts, 2)
     C(boundaryPts(i), boundaryPts(i)) = 1;
 end
 
 num_cagepts = size(cagePts, 2);
+% if i is a cage vertex
+% get the next cage vertex
+% C(i, i) = -1 and C(i, next) = 1
+% so that all cage vertices are equal
 for i = 1:num_cagepts
     next = mod(i, num_cagepts)+1;
     C(cagePts(i), cagePts(i)) = -1;
     C(cagePts(i), cagePts(next)) = 1;
 end
 
+% coordinates of the boundary points
 boundaryPts_loc = gm.Mesh.Nodes(:, boundaryPts);
 
 %%% -------------------for multiple direction---------------------------- %%%
@@ -185,11 +189,12 @@ end
 
 patch_x = []; %holds the x values of the triangle vertices
 patch_y = []; %holds the y values of the triangle vertices
-patch_color = [];
-face_gradient = [];
+patch_color = []; %holds the interpolated value of sol of the patch
+face_gradient = []; % holds the magnitude of the face gradient of the patch
 for i = 1:numelements
     v1_id = gm.Mesh.Elements(1, i);v2_id = gm.Mesh.Elements(2, i);v3_id = gm.Mesh.Elements(3, i);
     v1 = gm.Mesh.Nodes(:, v1_id);v2 = gm.Mesh.Nodes(:, v2_id);v3 = gm.Mesh.Nodes(:, v3_id);
+
     patch_x = [patch_x, [v1(1);v2(1);v3(1)]];
     patch_y = [patch_y, [v1(2);v2(2);v3(2)]];
     
@@ -242,19 +247,8 @@ colorbar;
 figure;
 contour(xGrid, yGrid, inside_outside, [0.5 0.5], 'k', 'LineWidth', 2);
 
-
-
-
 %%% -----------------------------------------------------------------%%%
 
-
-% plotting 
-% patch for linear interpolation
-%pdemesh(gm, "NodeLabels","on");
-%axis equal
-
-% imagesc([grad_xx, grad_yy]);
-% pdemesh(gm);
 exp_quantiles = quantile(face_gradient, [0.025, 0.75], "all");
 toobig = face_gradient>exp_quantiles(2);
 face_gradient(toobig) = exp_quantiles(2);
